8fd8ba70de00cfae6c90cf73962ce30bf2770feb
Hun425 <147483675+Hun425@users.noreply.github.com>
2025-06-04 10:48:09 +0900

    Add dev config server setup

---
+++ backend/modules/config-server/src/main/java/com/quizplatform/configserver/ConfigServerApplication.java
@@ -0,0 +1,16 @@
+package com.quizplatform.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * Spring Cloud Config 서버 애플리케이션
+ */
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
+}
